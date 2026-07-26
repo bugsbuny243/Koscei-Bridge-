d4eaabe454f00670e3d018c114cced010d12c8da
Onur <cndy121@gmail.com>
2026-07-26 21:21:09 +0300

    Public product: route cases to decisions and add creator publications

--- koschei/api/internal/http/dossier_routes.go
+++ koschei/api/internal/http/dossier_routes.go
@@ -8,10 +8,12 @@ import (
 
 func registerDossierRoutes(mux *http.ServeMux, h *handlers.Handler) {
 	mux.HandleFunc("/api/v1/dossier/", requiresDB(h, h.DossierAccess(method(http.MethodPost, h.DossierExport))))
+	mux.HandleFunc("/api/v1/dossier/publications", requiresDB(h, h.DossierPublicationAccess(method(http.MethodPost, h.CustomerDossierPublication))))
+	mux.HandleFunc("/api/v1/program-risks/publications", requiresDB(h, h.DossierPublicationAccess(method(http.MethodPost, h.ProgramRiskPublication))))
 	mux.HandleFunc("/dossier/", requiresDB(h, method(http.MethodGet, h.DossierPage)))
-	mux.HandleFunc("/case/", requiresDB(h, method(http.MethodGet, h.PublicCaseOperationalPage)))
+	mux.HandleFunc("/case/", requiresDB(h, method(http.MethodGet, h.PublicCaseSummaryPage)))
 	mux.HandleFunc("/program-risk/", requiresDB(h, method(http.MethodGet, h.PublicProgramRiskPage)))
-	mux.HandleFunc("/api/public/cases", requiresDB(h, method(http.MethodGet, h.PublicDossierCases)))
+	mux.HandleFunc("/api/public/cases", requiresDB(h, method(http.MethodGet, h.PublicSecurityCases)))
 	mux.HandleFunc("/api/public/program-risks", requiresDB(h, method(http.MethodGet, h.PublicProgramRisks)))
 	mux.HandleFunc("/api/public/program-risks/", requiresDB(h, method(http.MethodGet, h.PublicProgramRiskItem)))
 	mux.HandleFunc("/api/public/soc/feed", requiresDB(h, method(http.MethodGet, h.PublicUnifiedSOCFeed)))
