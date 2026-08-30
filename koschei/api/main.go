67d45727541b1236fda79f2c3e6cc2109fe50078
Onur <cndy121@gmail.com>
2026-08-30 19:46:50 +0300

    fix(lang): route mobile downloads through one-time tickets

--- koschei/api/main.go
+++ koschei/api/main.go
@@ -132,7 +132,7 @@ func main() {
 	staticDir := resolveStaticDir(os.Getenv("STATIC_DIR"))
 	log.Printf("static public path: %s", staticDir)
 	baseHandler := englishPublicHTML(apihttp.NewServer(conn, dbInitError, os.Getenv("ADMIN_PASSWORD"), firstEnv("CORS_ORIGIN", "CORS_ALLOWED_ORIGIN"), staticDir, apihttp.WithReadDB(readConn), apihttp.WithCache(appCache), apihttp.WithSolanaRPC(solanaRPC), apihttp.WithJobStore(jobStore), apihttp.WithJobQueue(jobQueue)))
-	handler := piLangHostSurface(baseHandler, staticDir)
+	handler := piLangMobileDownloadSurface(piLangHostSurface(baseHandler, staticDir), staticDir)
 	server := newHTTPServer(port, handler)
 
 	serverErrors := make(chan error, 1)
